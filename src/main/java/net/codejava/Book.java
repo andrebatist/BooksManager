cd33779b0f24dacc96de082f0a1f89e4c036ad9a
aplaksin <aplaksin@ecocompany.ru>
2020-01-13 16:20:26 +0300

    first commit

---
+++ src/main/java/net/codejava/Book.java
@@ -0,0 +1,51 @@
+package net.codejava;
+
+public class Book {
+    private Integer bookId;
+    private String title;
+    private String author;
+    private float price;
+
+    public Book() {
+
+    }
+
+    public Book(Integer bookId, String title, String author, float price) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
