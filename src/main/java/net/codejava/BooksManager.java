6185661de68f4175c5b6c6d4ce5f1c21709e062a
aplaksin <aplaksin@ecocompany.ru>
2020-01-14 15:31:08 +0300

    db all CRUD operations

--- src/main/java/net/codejava/BooksManager.java
+++ src/main/java/net/codejava/BooksManager.java
@@ -3,22 +3,70 @@ package net.codejava;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
 
 public class BooksManager {
+
+    static EntityManagerFactory factory;
+    static EntityManager entityManager;
+
     public static void main(String[] args) {
+        begin();
+       // create();
+       // update();
+       // find();
+       // query();
+        remove();
+        end();
+    }
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("BookUnit");
-        EntityManager entityManager = factory.createEntityManager();
-
+    private static void begin() {
+        factory = Persistence.createEntityManagerFactory("BookUnit");
+        entityManager = factory.createEntityManager();
         entityManager.getTransaction().begin();
+    }
+
+    private static void remove() {
+        Integer primaryKey = 3;
+        Book reference = entityManager.getReference(Book.class, primaryKey);
+        entityManager.remove(reference);
+    }
+
+    private static void query() {
+        String jpql = "Select b From Book b Where b.price < 30";
+        Query query = entityManager.createQuery(jpql);
+        @SuppressWarnings("unchecked")
+        List<Book> listBooks = query.getResultList();
+        listBooks.forEach(book -> System.out.println(book.getTitle() + " " + book.getAuthor() + " " + book.getPrice()));
+    }
+
+    private static void find() {
+        Integer primaryKey = 1;
+        Book book = entityManager.find(Book.class, primaryKey);
+        System.out.println(book.getTitle());
+        System.out.println(book.getAuthor());
+        System.out.println(book.getPrice());
+    }
+
+    private static void create() {
         Book newBook = new Book();
-//        newBook.setTitle("Effective Java");
-//        newBook.setAuthor("Joshua Bloch");
-//        newBook.setPrice(39);
-        newBook.setTitle("Book");
-        newBook.setAuthor("Author");
-        newBook.setPrice(99);
+        newBook.setTitle("Thinking in Java");
+        newBook.setAuthor("Bruce Eckel");
+        newBook.setPrice(45);
         entityManager.persist(newBook);
+    }
+
+    private static void update() {
+        Book existBook = new Book();
+        existBook.setBookId(1);
+        existBook.setTitle("Effective Java (2nd edition)");
+        existBook.setAuthor("Joshua Bloch");
+        existBook.setPrice(19);
+        entityManager.merge(existBook);
+    }
+
+    private static void end() {
         entityManager.getTransaction().commit();
         entityManager.close();
         factory.close();
